cf6c4ccc3c428095bdc37f948016b42fc0bae9a0
Taís Dias <taisayana@hotmail.com>
2023-03-25 21:16:52 +0000

    Criar um programa que faça o computador jogar Jokenpô com você.

--- jokenpô_2.0.py
+++ jokenpô_2.0.py
@@ -16,6 +16,12 @@ while resp == "":
     opcoes = ["PEDRA", "PAPEL", "TESOURA"]
     random.choice(opcoes[pc])
 
+    elogio = random.randint(0, 3)
+    elogios = ["Você é fera, hein? Você pratica o dia todo?", "Você joga muito bem! Me conta o segredo?",
+               "Vou ficar de olho para ver se você não está 'roubando' haha",
+               "Quando eu crescer, quero jogar como você!"]
+    random.choice(elogios)
+
     print("""Suas opções:
     [ 0 ] PEDRA
     [ 1 ] PAPEL
@@ -37,15 +43,16 @@ while resp == "":
 
     if pc == 0 and jogador == 1 or pc == 1 and jogador == 2:
         print("JOGADOR VENCE")
-        print("Parabéns! Você joga MUITO BEM!")
+        print(elogios[elogio])
     elif pc == 0 and jogador == 2:
         print("COMPUTADOR VENCE ", end="")
+        print(emoji.emojize(":sunglasses:", language="alias"))
     elif jogador == 0 and pc == 1 or jogador == 1 and pc == 2:
-        print("COMPUTADOR VENCE ", end ="")
+        print("COMPUTADOR VENCE ", end="")
         print(emoji.emojize(":sunglasses:", language="alias"))
     elif jogador == 0 and pc == 2:
         print("JOGADOR VENCE")
-        print("Parabéns! Você joga MUITO BEM!")
+        print(elogios[elogio])
     elif jogador == pc:
         print("EMPATE")
         print("Humm... precisamos desempatar! Estamos bons, hein?")
